3fb3f6fee44081bd7d225c77ea36c0556166642d
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-02-17 08:12:01 +0530

    Add files via upload

---
+++ PrimeNumbers.cs
@@ -0,0 +1,40 @@
+﻿//Write a program to find all prime number between two values inputted by user
+using System;
+
+class PrimeNumbers
+{
+    static void Main()
+    {
+        int start, end;
+
+        Console.Write("Enter first number: ");
+        start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        end = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Prime numbers between " + start + " and " + end + " are:");
+
+        for (int i = start; i <= end; i++)
+        {
+            if (i <= 1)
+                continue;
+
+            bool isPrime = true;
+
+            for (int j = 2; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.ReadLine();
+    }
+}
---
+++ SumOfNumbers.cs
@@ -0,0 +1,24 @@
+﻿//Write a program to calculate sum of number inputted by the user.
+using System;
+
+class SumOfNumbers
+{
+    static void Main()
+    {
+        int n, num, sum = 0;
+
+        Console.Write("How many numbers do you want to enter? ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write("Enter number " + i + ": ");
+            num = Convert.ToInt32(Console.ReadLine());
+            sum = sum + num;
+        }
+
+        Console.WriteLine("The sum of the numbers is: " + sum);
+
+        Console.ReadLine();
+    }
+}
