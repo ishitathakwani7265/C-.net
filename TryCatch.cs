e32b15efcc39c3e558d364c0b1889fd5ef0a942c
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-04-07 08:38:50 +0530

    Add files via upload

---
+++ TryCatch.cs
@@ -0,0 +1,34 @@
+﻿/*
+Write a program to demonstrate the use of try and catch in C#.
+*/
+
+using System;
+
+class ExceptionDemo
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int a, b, c;
+
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a / b;
+
+            Console.WriteLine("Result = " + c);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
