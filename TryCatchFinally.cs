45953877a24c98fcf166f502ac1c054bfd0ebd5b
ishitathakwani7265 <ishita.thakwani129809@marwadiuniversity.ac.in>
2026-04-07 08:39:20 +0530

    Add files via upload

---
+++ TryCatchFinally.cs
@@ -0,0 +1,38 @@
+﻿/*
+Write a program to demonstrate the use of try, catch and finally in C#.
+*/
+
+using System;
+
+class ExceptionDemo
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int a, b, c;
+
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a / b;
+
+            Console.WriteLine("Result = " + c);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input");
+        }
+        finally
+        {
+            Console.WriteLine("Program ended");
+        }
+    }
+}
